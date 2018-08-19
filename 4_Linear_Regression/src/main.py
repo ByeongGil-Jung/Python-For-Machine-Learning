704181e53e9255169efe2b8bf10b07285ba1006b
ByeongGil-Jung <jbkcose@gmail.com>
2018-08-20 01:41:15 +0900

    [18-08-20] - gradient descent   - performance measure - adding .gitignore

--- 4_Linear_Regression/src/main.py
+++ 4_Linear_Regression/src/main.py
@@ -49,6 +49,55 @@
 
 """
 
+"""
+
+[ performance_measure_lec.ipynb ]
+
+
+== (1) :: Performance Measure ==
+
+- 만든 모델이 얼마나 실제 값을 잘 대변해주는가를 상대적으로 비교를 하기 위해 만든 지표
+
+ 여러가지 지표들이 존재
+  - Mean Absolute Error
+    :: 잔차의 절대값을 sum 하는 방식
+       (0 에 가까울 수록 높은 적합도를 가짐)
+  
+  - Root Mean Squared Error (RMSE)
+  >> 가장 많이 사용되는 방법
+    :: 잔차 제곱의 sum 의 루트
+       (0 에 가까울 수록 높은 적합도를 가짐)
+  
+  - R-Squared
+    :: 기존 y 값과 예측 y^ 값과의 차이를 상관계수로 표현한 것
+       (1 에 가까울 수록 높은 적합도를 가짐) 
+
+
+== (2) :: Training & Test Data Set ==
+
+- Training 한 데이터로 다시 Test 를 할 경우, Training 데이터에 과도하게 fitting 된 모델을 사용하게 될 가능성이 높다.
+  >> 따라서 새로운 데이터가 출현했을 때, 기존 모델과의 차이가 존재함
+- 모델은 새로운 데이터가 처리 가능하도록 generalize 되야 함
+- 이를 위해 Training Set 과 Test Set 을 분리함
+
+< 과정 >
+ 1. 데이터 set 을 Training set 과 Test set 으로 나눔
+ 2. Training set 을 가지고 모델을 만듬
+ 3. 완성된 모델을 RMSE 와 같은 지표로 평가한다.
+ (weight 의 값에 따라 model 이 다양하게 생성되므로, Test set 으로 평가한다.)
+
+
+== (3) :: Hold-out Method(Sampling) ==
+
+>> Training data set 과 Test data set 을 나누는 과정
+
+- 데이터를 Training 과 Test 로 나눠서 모델을 생성하고 테스트하는 기법
+- 가장 일반적인 모델 생성을 위한 데이터 랜덤 샘플링 기법
+- Training 과 Test 를 나누는 비율은 데이터의 크기에 따라 다름
+- 일반적으로 Training data 2/3, Test data 1/3 을 활용함
+
+"""
+
 
 def main():
     return None
